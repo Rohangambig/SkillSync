495978d20c72c6bc026fa29ac7a89ef24e157aa3
Rohangambig <120889961+Rohangambig@users.noreply.github.com>
2025-05-31 15:50:19 +0530

    login page functionality

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import axios from "axios";
 import { useState } from "react";
 import style from "./page.module.css";
 import Link from "next/link";
@@ -6,24 +7,62 @@ import Link from "next/link";
 export default function LoginPage() {
   const [togglePassword, setTogglePass] = useState("password");
 
+  // Handling form data
+  const handleForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-//   handling show password or not
-const handleToggle = () => {
-    if(togglePassword === 'password')  setTogglePass('text');
-    else setTogglePass('password');
-}
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    const data = {
+      email: formData.get("email"),
+      employeeId: formData.get("employeeId"),
+      password: formData.get("password"),
+    };
+
+    try {
+      const result = await axios.post("http://localhost:5000/login", data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(result.data);
+    } catch (err) {
+      console.error("Error in sending login form", err);
+    }
+  };
+
+  //   handling show password or not
+  const handleToggle = () => {
+    if (togglePassword === "password") setTogglePass("text");
+    else setTogglePass("password");
+  };
 
   return (
-    <form className={style.formContainer}>
+    <form className={style.formContainer} onSubmit={handleForm}>
       <h3>Login Page</h3>
       <label>Email id</label>
-      <input type="text" placeholder="enter your email id"></input>
+      <input type="text" placeholder="enter your email id" name="email"></input>
       <label>Employee ID</label>
-      <input type="text" placeholder="enter employee id"></input>
+      <input
+        type="text"
+        placeholder="enter employee id"
+        name="employeeId"
+      ></input>
       <label>Password</label>
-      <input type={togglePassword} placeholder="enter your password"></input>
+      <input
+        type={togglePassword}
+        placeholder="enter your password"
+        name="password"
+      ></input>
       <div className={style.togglePassword}>
-        <div style={{ display: 'flex' ,justifyContent:'center',alignItems:'center'}}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <input type="checkbox" onClick={handleToggle}></input>
           <span>show password</span>
         </div>
